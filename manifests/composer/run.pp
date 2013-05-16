1fcceb8660e9a4f6a01de88460aca56ce01bed1d
Juan Treminio <jtreminio@gmail.com>
2013-05-16 13:35:29 -0500

    Support for different composer install location

--- manifests/composer/run.pp
+++ manifests/composer/run.pp
@@ -2,9 +2,8 @@ define php::composer::run (
     $command = 'install',
     $path
 ) {
-    exec { "composer-${path}-${command}":
-        command => "composer ${command} --working-dir ${path}",
-        path    => "${php::composer::install_location}",
-        require => Class['php::composer'],
+    exec { "composer-${path}-${command}-${php::composer::composer_location}":
+        command => "${php::composer::composer_filename} ${command} --working-dir ${path}",
+        path    => "/usr/bin:/usr/sbin:/bin:/sbin:${php::composer::composer_location}",
     }
 }
