9dc4ebdae456e4e493a86e87063a19c217c7b023
Juan Treminio <jtreminio@gmail.com>
2013-05-16 21:32:10 -0500

    Requiring composer before ... running Composer

--- manifests/composer/run.pp
+++ manifests/composer/run.pp
@@ -5,5 +5,6 @@ define php::composer::run (
     exec { "composer-${path}-${command}-${php::composer::composer_location}":
         command => "${php::composer::composer_filename} ${command} --working-dir ${path}",
         path    => "/usr/bin:/usr/sbin:/bin:/sbin:${php::composer::composer_location}",
+        require => Class['php::composer']
     }
 }
