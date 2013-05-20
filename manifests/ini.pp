c3ed598afef62bd072f136c63428f19b13b10f79
Frank Stelzer <dev@frankstelzer.de>
2013-05-20 11:04:09 +0200

    removed hardcoded apache service dependency

--- manifests/ini.pp
+++ manifests/ini.pp
@@ -1,13 +1,14 @@
 define php::ini (
     $value    = '',
     $template = 'extra-ini.erb',
-    $target   = 'extra.ini'
+    $target   = 'extra.ini',
+    $service  = $php::service
 ) {
     file { "/etc/php5/conf.d/${target}" :
         ensure  => 'present',
         content => template("php/${template}"),
         require => Package['php'],
-        notify  => Service['apache'],
+        notify  => Service[$service],
     }
 
     file { "/etc/php5/cli/conf.d/${target}" :
