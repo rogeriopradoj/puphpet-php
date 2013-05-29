178dc9e55d17794123f3d064a4209059845582da
Juan Treminio <jtreminio@gmail.com>
2013-05-29 10:19:59 -0500

    Using snakes instead of snakeCase

--- manifests/ini/removeblock.pp
+++ manifests/ini/removeblock.pp
@@ -1,12 +1,12 @@
-define php::ini::removeBlock (
-    $blockName,
-    $iniFile
+define php::ini::removeblock (
+    $block_name,
+    $ini_file
 ) {
     $left_side_regex = 'perl -pi -w -e "s/^(\[?)'
     $right_side_regex = '(\]?)(.+\n)//"'
-    $cmd = "${left_side_regex}${blockName}${right_side_regex}"
+    $cmd = "${left_side_regex}${block_name}${right_side_regex}"
 
-    exec { "${cmd} ${iniFile}" :
+    exec { "${cmd} ${ini_file}" :
         path => '/usr/bin/',
         require => Package['php']
     }
